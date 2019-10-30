3ac02150920fe1652e33f4a75e1d7b45dcefa304
alci.barros <alcirbarros@hotmail.com.br>
2019-10-30 20:56:45 -0300

    adicionando tela do quiz ao projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: '**', redirectTo: 'home' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
